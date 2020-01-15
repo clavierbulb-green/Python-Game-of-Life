3c82e0122c6ea8f53d1e3f2aa1410230ae9746f5
clavierbulb-green <clavierbulb-green@protonmail.com>
2020-01-15 10:47:57 -0800

    Change input from flat list to nested list
    Using a 2-D list, with each inner list representing a row of squares on the
    game board, makes for easier math when referencing individual squares (i.e.
    don't need to convert from list index to x, y coords.).
    
    The width of the board is also contained in the board itself
    (i.e. simply the length of the first row), and does not have to be passed
    in as a separate parameter.
    
    Printing the output is also be made simpler (simply print one row at a
    time).

--- README.md
+++ README.md
@@ -3,25 +3,25 @@
 
 An implementation of Conway's Game of Life in Python.
 
-Written on a whim, to have fun and challenge myself to work out on my own 
-how to implement the algorithm for the game, in an efficient way; to try out 
-generators, nested functions, type annotation, etc. in Python; and to try 
-to make a fun little command-line app.
+Written on a whim, to have fun and challenge myself to work out on my own
+how to implement the algorithm for the game; to try out generators, nested
+functions, type annotation, etc. in Python; and to try to make a fun little
+command-line app.
 
-So far, consists only of a generator function that takes a flat list
-representation of an initial board of squares, and the width of the board,
-and yields a list representing the result of the next round of play.
+So far, consists only of a generator function that takes a nested list
+that represents an initial board of squares as a seed, and yields a list
+representing the result of the next round of play.
 A value of 1 indicates a 'live' square, whereas a value of 0 a 'dead' square.
 
 Example usage:
 ```
 >>> from life import game_of_life
->>> seed = [0, 0, 0, 1, 1, 1, 0, 0, 0]
->>> game = game_of_life(seed, width=3)
+>>> seed = [[0, 0, 0], [1, 1, 1], [0, 0, 0]]
+>>> game = game_of_life(seed)
 >>> next(game)
-[0, 0, 0, 1, 1, 1, 0, 0, 0]
+[[0, 0, 0], [1, 1, 1], [0, 0, 0]]
 >>> next(game)
-[0, 1, 0, 0, 1, 0, 0, 1, 0]
+[[0, 1, 0], [0, 1, 0], [0, 1, 0]]
 ```
 ---
 ### TODO:
--- life.py
+++ life.py
@@ -1,53 +1,60 @@
 from typing import List, Generator, Tuple
 
 
-def game_of_life(seed: List[int],
-                 width: int) -> Generator[List[int], None, None]:
-    ''' A generator function that takes a flat representation of a board
-    of squares as a seed and a board width and yields the result of an
-    additional round of Conway's Game of Life.'''
+def game_of_life(seed: List[List[int]]
+                 ) -> Generator[List[List[int]], None, None]:
+    ''' A generator function that takes a representation of a board
+    of squares as a seed and yields the result of an additional round
+    of Conway's Game of Life.'''
 
     NEIGHBOR_COORDS: List[Tuple[int, int]] = [(-1, -1), (0, -1), (1, -1),
                                               (-1, 0),           (1, 0),
                                               (-1, 1),  (0, 1),  (1, 1)]
 
-    def play_round(state: List[int]) -> List[int]:
-        def count_live_neighbors(i: int) -> int:
-            x: int = i % width
-            y: int = i // width
+    def play_round(state: List[List[int]]) -> List[List[int]]:
+        '''Returns a representation of the state that results from applying
+        one round of the Game of Life to an input state.
+        The rules of the Game are as follows:
+            A square/cell with
+            - 3 live neighbors stays 'alive', or becomes 'alive' if 'dead'
+            - 2 live neighbors maintains its state ('dead' or 'alive')
+            - any other number of live neighbors 'dies', or stays 'dead'
+        '''
+
+        def count_live_neighbors(x: int, y: int) -> int:
+            '''Returns the number of live squares that are neighbors (i.e.
+            adjacent horizontally, vertically, or diagonally) to a square
+            defined by given x and y coordinates in the current board state.'''
+
             num_live_neighbors: int = 0
 
             for (dx, dy) in NEIGHBOR_COORDS:
                 neighbor_x: int = (x + dx)
                 neighbor_y: int = (y + dy)
 
-                if neighbor_x not in range(width) or \
-                        neighbor_y not in range(len(state) // width):
+                if neighbor_x not in range(len(state[0])) or \
+                        neighbor_y not in range(len(state)):
                     continue
 
-                neighbor_index: int = neighbor_x + (neighbor_y * width)
-                if state[neighbor_index]:
+                if state[neighbor_y][neighbor_x]:
                     num_live_neighbors += 1
 
             return num_live_neighbors
 
-        next: List[int] = []
-        for (i, sq) in enumerate(state):
-            num_live_neighbors: int = count_live_neighbors(i)
-            # a square with 3 live neighbors stays alive/becomes alive
-            if num_live_neighbors == 3:
-                next.append(1)
-            # a square with 2 live neighbors maintains its state
-            elif num_live_neighbors == 2:
-                next.append(sq)
-            # else a cell dies/stays dead
-            else:
-                next.append(0)
-
+        next: List[List[int]] = [[] for row in range(len(state))]
+        for (y, row) in enumerate(state):
+            for (x, sq) in enumerate(row):
+                num_live_neighbors: int = count_live_neighbors(x, y)
+                if num_live_neighbors == 3:
+                    next[y].append(1)
+                elif num_live_neighbors == 2:
+                    next[y].append(sq)
+                else:
+                    next[y].append(0)
         return next
 
-    current: List[int] = seed
+    current: List[List[int]] = seed
     while True:
         yield current
-        next: List[int] = play_round(current)
+        next: List[List[int]] = play_round(current)
         current = next
